394a10338d374d46f5b94d61900fa74c38cc9792
Your Name <you@example.com>
2024-07-15 10:51:56 +0545

    initializing backend

---
+++ index.js
@@ -0,0 +1,65 @@
+// function expression
+
+// const { lstat } = require("fs");++++++++++
+
+function getRandomInt() {
+  return Math.random(Math.random() * 100);
+}
+
+const randomInt = getRandomInt();
+console.log(randomInt);
+
+// function declaration
+
+const randominttt = function () {
+  return Math.random(Math.random() * 100);
+};
+console.log(randominttt());
+
+// arrow function
+const randomIntt = () => Math.random(Math.random() * 100);
+console.log(randomIntt());
+
+const pet = {
+  name: "doggo",
+  age: 20,
+  likes: {
+    indoor: "sleep",
+    outdoor: "playing fetch",
+  },
+};
+
+console.log(
+  `My name is ${pet.name}, I am ${pet.age} years old and I like to ${pet.likes.indoor} inside and ${pet.likes.outdoor} outside.`
+);
+
+const {
+  name,
+  age,
+  likes: { indoor, outdoor },
+} = pet;
+
+console.log(name, age, indoor, outdoor);
+
+const name1 = (namelkdk) => {
+  return namelkdk;
+};
+
+console.log(name1(name));
+
+// spread operators
+
+const originalNumbers = { a: 1, b: 2 };
+const copiedObject = { ...originalNumbers };
+
+console.log(copiedObject);
+
+let users = [
+  { firstName: "ram", lastName: "dhital" },
+  { firstName: "ram", lastName: "dhital" },
+];
+let usernames = users.map(
+  (element) => `${element.firstName} ${element.lastName}`
+);
+
+console.log(usernames);
